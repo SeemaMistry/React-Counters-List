154acb212003a041e283acf036dcc5b8a5d48f66
SeemaMistry <seema.m.1996@gmail.com>
2021-12-11 14:12:47 -0500

    Use mapping to loop through an array and render array items in JSX list format.

--- counters-list/src/components/counter.js
+++ counters-list/src/components/counter.js
@@ -3,6 +3,7 @@ import React from 'react';
 export const Counter = () => {
   const state = {
     count: 0,
+    fruit: ['apples', 'bananas', 'cherry'],
   };
   // render the counter-object count and add an increment button next to it
   // set styling using bootstrap
@@ -12,6 +13,11 @@ export const Counter = () => {
     <>
       <span className={getBtnColour()}>{state.count}</span>
       <button className='btn btn-secondary btn-sm'>Increment</button>
+      <ul>
+        {state.fruit.map((fruit) => (
+          <li>{fruit}</li>
+        ))}
+      </ul>
     </>
   );
 
