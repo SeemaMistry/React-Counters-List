cd14ba2f70be5bc76b0cbdfaa8e52777337ee3e6
SeemaMistry <seema.m.1996@gmail.com>
2021-12-11 14:17:49 -0500

    Initalize a counters componenet, a component which creates a list of counters.

---
+++ counters-list/src/components/counters.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Counter } from './counter';
+
+export const Counters = () => {
+  return (
+    <div>
+      <Counter />
+      <Counter />
+
+      <Counter />
+
+      <Counter />
+    </div>
+  );
+};
