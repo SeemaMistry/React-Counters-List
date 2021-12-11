5997fd3bb9d77c0aaacaf408e457093e369d70df
SeemaMistry <seema.m.1996@gmail.com>
2021-12-11 15:44:54 -0500

    Conditionally render counters using renderCounters() call. If there are no counters, displayer text saying List is empty.

--- counters-list/src/components/counters.js
+++ counters-list/src/components/counters.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Counter } from './counter';
 
+// initalize a set of counters as an array in the counters-object
+// render a list of counters only if counters array > 0
 export const Counters = () => {
-  return (
-    <div>
-      <Counter />
-      <Counter />
+  const state = {
+    counters: [
+      { id: 0, value: 4 },
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+    ],
+  };
 
-      <Counter />
+  const renderCounters = () => {
+    if (state.counters.length === 0) return <h3>List is empty</h3>;
+    return state.counters.map((counter) => <Counter key={counter.id} />);
+  };
 
-      <Counter />
-    </div>
-  );
+  return <>{renderCounters()}</>;
 };
