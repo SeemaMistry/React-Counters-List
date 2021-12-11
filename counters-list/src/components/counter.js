4a4f8f5cb5dff7f976cbf652ea4b9d0868adb089
SeemaMistry <seema.m.1996@gmail.com>
2021-12-11 11:29:49 -0500

    Conditionally render count value className. If count is 0 then add warning-badge from bootstrap, else primary-badge.

--- counters-list/src/components/counter.js
+++ counters-list/src/components/counter.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 export const Counter = () => {
   const state = {
-    count: 100,
+    count: 0,
   };
   // render the counter-object count and add an increment button next to it
   // set styling using bootstrap
+
+  // for btns, render styling conditionally - if count > 0 or not add warning or primary badge
   return (
     <>
-      <span className='badge badge-primary m-2'>{state.count}</span>
+      <span className={getBtnColour()}>{state.count}</span>
       <button className='btn btn-secondary btn-sm'>Increment</button>
     </>
   );
+
+  function getBtnColour() {
+    let classes = 'badge m-2 badge-';
+    return (classes += state.count === 0 ? 'warning' : 'primary');
+  }
 };
