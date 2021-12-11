787623320511e41078a82e0734ce947e6ac7b43d
SeemaMistry <seema.m.1996@gmail.com>
2021-12-11 11:09:13 -0500

    Create state object for counter component. Dynamically render state.count to screen.

--- counters-list/src/components/counter.js
+++ counters-list/src/components/counter.js
@@ -1,9 +1,13 @@
-// import React from 'react';
+import React from 'react';
 
 export const Counter = () => {
+  const state = {
+    count: 100,
+  };
   return (
     <div>
       <h3>I am a counter</h3>
+      <p>I have a state of {state.count}</p>
     </div>
   );
 };
